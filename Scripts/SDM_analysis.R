76c2e15159c6e350b91177ae02e940c6a56bccc2
ssnell6 <ssnell@ad.unc.edu>
2019-03-08 15:14:10 -0500

    making maps for visual interpretation, GLM OCC

--- Scripts/SDM_analysis.R
+++ Scripts/SDM_analysis.R
@@ -106,8 +106,6 @@ for(i in sp_list){
  j = unique(sdm_input$ALPHA.CODE)
  plot = plot(roccurve, main = paste("AUC Curve for ", j, ".csv",   
                                      sep=""))
- # lines(rocpres, col = "red")
-#    }
   }
  write.csv(sdm_output, paste("sdm_output_", i, ".csv",   
                             sep=""), row.names = FALSE)
@@ -121,40 +119,81 @@ names(auc_df) = c("AOU", "AUC", "pres_AUC")
 # write.csv(auc_df, "Data/auc_df.csv", row.names = FALSE)
 test = dplyr::filter(auc_df, AUC > 0.75 & AUC < 1.0)
 
-#### Mapping for spp ####
-# the XY coordinates of species data
-coords <- CRS("+proj=longlat +datum=WGS84")
 
-# Determine geographic extent of our data using AOU = i
-max.lat <- ceiling(max(sdm_input$latitude))
-min.lat <- floor(min(sdm_input$latitude))
-max.lon <- ceiling(max(sdm_input$longitude))
-min.lon <- floor(min(sdm_input$longitude))
-geographic.extent <- extent(x = c(min.lon, max.lon, min.lat, max.lat))
-env.data = as.matrix(sdm_input[,c("latitude", "longitude", "elev.mean", "ndvi.mean", "bio.mean.bio1", "bio.mean.bio12")])
-env.data = raster(env.data)
+bbs_final_occ_ll$presence <- factor(bbs_final_occ_ll$presence,levels = c('1','0'), ordered = TRUE)
 
-data(wrld_simpl)
-# Plot the base map
-plot(wrld_simpl, 
+#### MAPS #####
+setwd("Figures/maps/")
+pdf('SDM_glm_occ_maps.pdf', height = 8, width = 10)
+par(mfrow = c(2, 3)) # makes plots too small
+
+# making sdm plot maps
+for(i in sp_list){
+  sdm_output = c()
+  
+  bbs_sub <- filter(bbs_final_occ_ll, Aou == i)
+  temp <- filter(all_env, stateroute %in% bbs_sub$stateroute)
+  sdm_input <- left_join(bbs_sub, temp, by = "stateroute")
+  sdm_input = na.omit(sdm_input)
+  j = unique(sdm_input$ALPHA.CODE)
+  
+  # print(length(sdm_input$stateroute))
+  if(length(unique(sdm_input$stateroute)) > 40){
+    #   if(levels(as.factor(sdm_input$presence)) > 1){
+    glm_occ <- glm(cbind(sp_success, sp_fail) ~ elev.mean + ndvi.mean +bio.mean.bio1 + bio.mean.bio12, family = binomial(link = logit), data = sdm_input)
+    glm_pres <- glm(presence ~ elev.mean + ndvi.mean +bio.mean.bio1 + bio.mean.bio12, family = binomial(link = logit), data = sdm_input)
+    predocc <- predict(glm_occ,type=c("response"))
+    predpr <- predict(glm_pres,type=c("response"))
+    sdm_output = cbind(sdm_input,predpr, predocc)
+  
+    # Determine geographic extent of our data using AOU = i
+  max.lat <- ceiling(max(sdm_input$latitude))
+  min.lat <- floor(min(sdm_input$latitude))
+  max.lon <- ceiling(max(sdm_input$longitude))
+  min.lon <- floor(min(sdm_input$longitude))
+
+  geographic.extent <- extent(x = c(min.lon, max.lon, min.lat, max.lat))
+
+
+  predocc.r <- SpatialPointsDataFrame(coords = sdm_output[,c("longitude", "latitude")],
+   data = sdm_output[,c("latitude", "longitude","Aou","predocc")], 
+   proj4string = CRS("+proj=laea +lat_0=45.235 +lon_0=-106.675 +units=km"))
+  r = raster(predocc.r)
+  plot.r = rasterize(predocc.r, r)
+  # plot.r.2 = mask(plot.r, wrld_simpl)
+
+
+  data(wrld_simpl)
+  # Plot the base map
+  plot(wrld_simpl, 
      xlim = c(min.lon, max.lon),
      ylim = c(min.lat, max.lat),
      axes = TRUE, 
-     col = "grey95")
+     col = "grey95", main = paste("SDM plot for ", j, sep=""))
+  
+  plot(plot.r$predocc, add = TRUE)
+  # Add the points for individual observation if necessary
+  # points(x = sdm_input$longitude, y = sdm_input$latitude, col = sdm_input$presence, pch = 20, cex = 0.75)
 
-# Add the points for individual observation
-points(x = sdm_input$longitude, 
-       y = sdm_input$latitude, 
-       col = "olivedrab", 
-       pch = 20, 
-       cex = 0.75)
-box()
+  box()
+  }
 
-# Build species distribution model
-obs.data = sdm_input[,c("longitude","latitude")]
+}
 
-rbrick = brick(env.data)
-plot(rbrick)
+dev.off()
+
+
+
+env.data = as.matrix(sdm_input[,c("latitude", "longitude", "elev.mean", "ndvi.mean", "bio.mean.bio1", "bio.mean.bio12")])
+
+env.proj = SpatialPointsDataFrame(coords = sdm_input[,c("longitude", "latitude")],
+                                  data = sdm_input[,c("latitude", "longitude", "elev.mean", "ndvi.mean", "bio.mean.bio1", "bio.mean.bio12")], 
+                                  proj4string = CRS("+proj=laea +lat_0=45.235 +lon_0=-106.675 +units=km"))
+r = raster(env.proj)
+env.proj.raster = rasterize(env.proj, r)
+env.stack = raster::stack(env.proj.raster@data$elev.mean, env.proj.raster@data$ndvi.mean)
+
+
 
 # plot GLM occ v pres
 r1 = ggplot(auc_df, aes(x = AUC, y = pres_AUC)) +theme_classic()+ theme(axis.title.x=element_text(size=36, vjust = 2),axis.title.y=element_text(size=36, angle=90, vjust = 2)) + xlab(bquote("Occupancy AUC")) + ylab(bquote("Presence AUC"))+
