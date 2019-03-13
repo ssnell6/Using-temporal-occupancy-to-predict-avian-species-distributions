8ed237fed7ca817c7d5915ce1ce05ce1e6b49fdc
ssnell6 <ssnell@ad.unc.edu>
2019-03-13 14:57:19 -0400

    working on occ pres plot, incorporating gams:

--- Scripts/SDM_analysis.R
+++ Scripts/SDM_analysis.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(glm2)
+library(gam)
 library(ggplot2)
 library(dismo)
 library(maptools)
@@ -12,8 +13,9 @@ bbs_occ_sub = bbs_occ %>% filter(Aou > 2880) %>%
   filter(Aou != 7010)
 
 exp_pres = read.csv("Data/expect_pres.csv", header = TRUE)
-exp_pres = exp_pres[,c("stateroute","spAOU")]
+exp_pres = exp_pres[,c("stateroute","spAOU")] 
 traits = read.csv("Data/Master_RO_Correlates.csv", header = TRUE)
+bsize = read.csv("data/DunningBodySize_old_2008.11.12.csv", header = TRUE)
 lat_long = read.csv("Data/latlongs.csv", header = TRUE)
 tax_code = read.csv("Data/Tax_AOU_Alpha.csv", header = TRUE)
 bi_env = read.csv("Data/all_env.csv", header = TRUE)
@@ -21,6 +23,7 @@ bi_means = bi_env[,c("stateroute","mat.mean", "elev.mean", "map.mean", "ndvi.mea
 env_bio = read.csv("Data/env_bio.csv", header = TRUE)
 env_bio = na.omit(env_bio)
 env_bio_sub = env_bio[,c(1, 21:39)]
+
 # env_bio[,c("stateroute","bio.mean.bio4", "bio.mean.bio5", "bio.mean.bio6", "bio.mean.bio13", "bio.mean.bio14")]
 
 all_env = left_join(bi_means, env_bio_sub, by = "stateroute")
@@ -42,9 +45,9 @@ bbs_final_occ = filter(num_occ,nn > 1)
 bbs_occ_code = left_join(bbs_final_occ, tax_code, by = c("Aou" = "AOU_OUT"))
 
 # 319 focal species
-# bi_focal_spp = filter(bbs_occ_code, Aou %in% exp_pres$spAOU)
+bbs_focal_spp = filter(bbs_occ_code, Aou %in% tax_code$AOU_OUT)
   
-bbs_final_occ_ll = left_join(bi_focal_spp , lat_long, by = "stateroute")
+bbs_final_occ_ll = left_join(bbs_focal_spp, lat_long, by = "stateroute")
 bbs_final_occ_ll = bbs_final_occ_ll[,c("Aou", "stateroute", "occ", "presence", "ALPHA.CODE",
                                        "latitude", "longitude")]
 bbs_final_occ_ll$sp_success = 15 * bbs_final_occ_ll$occ
@@ -68,7 +71,6 @@ sdm_input_global <- left_join(bbs_final_occ_ll, all_env, by = "stateroute")
 
 
 test = cor(na.omit(sdm_input_global))
-
 corrplot(test)
 
 
@@ -89,19 +91,23 @@ for(i in sp_list){
   sdm_input <- left_join(bbs_sub, temp, by = "stateroute")
   sdm_input = na.omit(sdm_input)
   # print(length(sdm_input$stateroute))
-  if(length(unique(sdm_input$stateroute)) > 40){
+  if(length(unique(sdm_input$stateroute)) > 40 & length(unique(sdm_input$presence)) >1){
  #   if(levels(as.factor(sdm_input$presence)) > 1){
-  glm_occ <- glm(cbind(sp_success, sp_fail) ~ elev.mean + ndvi.mean +bio.mean.bio1 + bio.mean.bio12, family = binomial(link = logit), data = sdm_input)
-  glm_pres <- glm(presence ~ elev.mean + ndvi.mean +bio.mean.bio1 + bio.mean.bio12, family = binomial(link = logit), data = sdm_input)
-  predocc <- predict(glm_occ,type=c("response"))
-  predpr <- predict(glm_pres,type=c("response"))
- sdm_output = cbind(sdm_input,predpr, predocc)
+    glm_occ <- glm(cbind(sp_success, sp_fail) ~ elev.mean + ndvi.mean +bio.mean.bio1 + bio.mean.bio12, family = binomial(link = logit), data = sdm_input)
+    glm_pres <- glm(presence ~ elev.mean + ndvi.mean +bio.mean.bio1 + bio.mean.bio12, family = binomial(link = logit), data = sdm_input)
+   # gam_occ <- gam(cbind(sp_success, sp_fail) ~ elev.mean + ndvi.mean +bio.mean.bio1 + bio.mean.bio12, family = binomial(link = logit), data = sdm_input)
+   # gam_pres <- gam(presence ~ elev.mean + ndvi.mean +bio.mean.bio1 + bio.mean.bio12, family = binomial(link = logit), data = sdm_input)
+    pred_glm_occ <- predict(glm_occ,type=c("response"))
+    pred_glm_pr <- predict(glm_pres,type=c("response"))
+    # pred_gam_occ <- predict(gam_occ,type=c("response"))
+    # pred_gam_pr <- predict(gam_pres,type=c("response"))
+    sdm_output = cbind(sdm_input,pred_glm_pr, pred_glm_occ) #, pred_gam_pr, pred_gam_occ)
  
- roccurve <- roc(sdm_output$occ ~ sdm_output$predocc)
- auc =  roc(sdm_output$occ ~ sdm_output$predocc)$auc[1]
+ roccurve <- roc(sdm_output$occ ~ sdm_output$pred_glm_occ)
+ auc =  roc(sdm_output$occ ~ sdm_output$pred_glm_occ)$auc[1]
  
- rocpres <- roc(sdm_output$presence ~ sdm_output$predpr)
- auc_pres =  roc(sdm_output$presence ~ sdm_output$predpr)$auc[1]
+ rocpres <- roc(sdm_output$presence ~ sdm_output$pred_glm_pr)
+ auc_pres =  roc(sdm_output$presence ~ sdm_output$pred_glm_pr)$auc[1]
  
  auc_df = rbind(auc_df, c(i, auc, auc_pres))
  j = unique(sdm_input$ALPHA.CODE)
@@ -145,9 +151,11 @@ for(i in unique(sp_list_bigauc$Aou)){
     #   if(levels(as.factor(sdm_input$presence)) > 1){
     glm_occ <- glm(cbind(sp_success, sp_fail) ~ elev.mean + ndvi.mean +bio.mean.bio1 + bio.mean.bio12, family = binomial(link = logit), data = sdm_input)
     glm_pres <- glm(presence ~ elev.mean + ndvi.mean +bio.mean.bio1 + bio.mean.bio12, family = binomial(link = logit), data = sdm_input)
-    predocc <- predict(glm_occ,type=c("response"))
-    predpr <- predict(glm_pres,type=c("response"))
-    sdm_output = cbind(sdm_input,predpr, predocc)
+    
+    pred_glm_occ <- predict(glm_occ,type=c("response"))
+    pred_glm_pr <- predict(glm_pres,type=c("response"))
+    
+    sdm_output = cbind(sdm_input,pred_glm_pr, pred_glm_occ)
   
     # Determine geographic extent of our data using AOU = i
   max.lat <- ceiling(max(sdm_input$latitude))
@@ -204,13 +212,20 @@ num_routes = bbs_final_occ_ll %>% group_by(Aou) %>%
   filter(., n >= 40)
 
 auc_df_merge = left_join(auc_df, num_routes, by = c("AOU" = "Aou"))
-# plot GLM occ v pres + geom_label(data = auc_df, aes(x = AUC, y = pres_AUC, label = AOU))
-# , to change size of points
-r1 = ggplot(auc_df, aes(x = AUC, y = pres_AUC)) +theme_classic()+ theme(axis.title.x=element_text(size=36, vjust = 2),axis.title.y=element_text(size=36, angle=90, vjust = 2)) + xlab(bquote("Occupancy AUC")) + ylab(bquote("Presence AUC"))+ geom_abline(intercept = 0, slope = 1, col = "black", lwd = 1.5)  + 
-  geom_point(shape=16, aes(size = auc_df_merge$n))  + geom_smooth(method='lm', se=FALSE, col="blue",linetype="longdash", lwd =2.5) +
-  theme(axis.text.x=element_text(size = 32),axis.ticks=element_blank(), axis.text.y=element_text(size=32))+ scale_colour_manual("", values=c("#dd1c77","#2ca25f","dark gray")) +
+auc_df_traits = left_join(auc_df_merge, traits, by = "AOU") 
+# plot GLM occ v pres 
+# + geom_label(data = auc_df, aes(x = AUC, y = pres_AUC, label = AOU))
+r1 = ggplot(auc_df_traits, aes(x = AUC, y = pres_AUC)) +theme_classic()+ theme(axis.title.x=element_text(size=36, vjust = 2),axis.title.y=element_text(size=36, angle=90, vjust = 2)) + xlab(bquote("Occupancy AUC")) + ylab(bquote("Presence AUC"))+ geom_abline(intercept = 0, slope = 1, col = "black", lwd = 1.5)  + 
+  geom_point(shape=16, aes(size = auc_df_traits$n, col = auc_df_traits$log_Brange_Area))  + geom_smooth(method='lm', se=FALSE, col="blue",linetype="longdash", lwd =2.5) 
+  theme(axis.text.x=element_text(size = 32),axis.ticks=element_blank(), axis.text.y=element_text(size=32)) +
   guides(colour = guide_legend(override.aes = list(shape = 15))) +
-  theme(legend.title=element_blank(), legend.text=element_text(size=36), legend.position = c(0.8,0.2), legend.key.width=unit(2, "lines"), legend.key.height =unit(3, "lines")) 
-ggsave("Figures/Occ_Pres.pdf", height = 8, width = 12)
+  theme(legend.title=element_blank(), legend.text=element_text(size=15), legend.position = c(0.1,0.8), legend.key.width=unit(2, "lines"), legend.key.height =unit(3, "lines")) 
+# ggsave("Figures/Occ_Pres_troph.pdf", height = 8, width = 12)
 
+auc_df_traits$diff = auc_df_traits$AUC - auc_df_traits$pres_AUC
+occ_sub = subset(auc_df_traits,diff >= 0)
+pres_sub = subset(auc_df_traits,diff < 0)
 
+num_pres = bbs_final_occ_ll %>%
+  group_by(Aou) %>%
+  summarize(sum = sum(as.numeric(presence)))
